a597073f9ba4326c51a583225ebcb591f7b31c7d
Klevi7 <klevi.mehmeti@hotmail.com>
2025-07-14 23:59:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Simulate fullscreen click scroller
+document.getElementById('desktop-click-fullscreen').addEventListener('click', () => {
+  alert('Expand to fullscreen animation here...');
+});
+
+// Placeholder for fullscreen mobile interstitial
+document.getElementById('mobile-fullscreen').addEventListener('click', () => {
+  alert('Mobile fullscreen interstitial would appear.');
+});
